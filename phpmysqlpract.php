9d555dfa5551ab6da037d2c8a70e9e4b697c6547
rahulchidu <49508732+rahulchidu@users.noreply.github.com>
2019-04-11 10:54:06 +0530

    Add files via upload

---
+++ phpmysqlpract.php
@@ -0,0 +1,44 @@
+<html>
+<head></head>
+<body>
+
+<form action="phpmysqlpract.php" method="post">
+Name
+<input type="text" name="fname"> <br>
+Password
+<input type="password" name="pwd"> <br>
+<input type="submit" name="submit" value="submit"> <br>
+</form>
+<?php
+		$dbhost = "localhost";
+		$dbuser = "root";
+		$dbpass = "";
+		$db = "sample";
+		$conn =mysqli_connect($dbhost, $dbuser, $dbpass,$db) ;
+		$query= "SELECT * FROM techbee";
+		$result=mysqli_query($conn,$query);
+														//display name
+		while($value=mysqli_fetch_array($result))   //to display all rows n columns
+		{
+			foreach($rows as $i=>$j)
+			{
+				echo "$i=>$rows[$i]<br>";
+			}
+			//echo $value['Name']. "<br>";
+			//echo $value['User_ID']. "<br>";
+		}
+		if(isset($POST['submit']))
+		{
+			$name=$_POST['fname'];
+			$pwd=$_POST['pwd'];
+			echo "$name,$pwd";
+			$query1= "insert into techbee values('$name','$pwd')";
+			$result1=mysqli_query($conn,$query1);
+		}	
+?>
+
+
+
+
+</body>
+</html>
